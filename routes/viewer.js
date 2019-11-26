a53ab36ce29766f7a7b76b2f8987e4d7e767454c
AgustinBarg <agustinbarg@gmail.com>
2019-11-26 18:27:34 -0300

    initial commit

---
+++ routes/viewer.js
@@ -0,0 +1,51 @@
+var express = require('express');
+const fs = require('fs')
+var router = express.Router();
+
+/* GET viewer. */
+router.get('/rick-and-morty/:episode', function(req, res, next) {
+  res.render('viewer', { video: req.params.episode});
+  console.log(req.params.episode)
+});
+
+router.get('/videos/rick-and-morty/:episode', function(req, res) {
+
+  const videoPath = 'public/rick-and-morty/' + req.params.episode + '.mp4'
+  const stat = fs.statSync(videoPath)
+  const fileSize = stat.size
+  const range = req.headers.range
+
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-")
+    const start = parseInt(parts[0], 10)
+    const end = parts[1]
+      ? parseInt(parts[1], 10)
+      : fileSize-1
+
+    if(start >= fileSize) {
+      res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+      return
+    }
+
+    const chunksize = (end-start)+1
+    const file = fs.createReadStream(videoPath, {start, end})
+    const head = {
+      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunksize,
+      'Content-Type': 'video/mp4',
+    }
+
+    res.writeHead(206, head)
+    file.pipe(res)
+  } else {
+    const head = {
+      'Content-Length': fileSize,
+      'Content-Type': 'video/mp4',
+    }
+    res.writeHead(200, head)
+    fs.createReadStream(videoPath).pipe(res)
+  }
+})
+
+module.exports = router;
